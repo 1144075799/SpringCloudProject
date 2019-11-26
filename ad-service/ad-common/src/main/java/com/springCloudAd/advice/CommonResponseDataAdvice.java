6aaace30887dadd15223e17a2dd3aa4c28dfbf4c
1144075799 <1144075799@qq.com>
2019-11-26 15:10:47 +0800

    common 统一响应处理

---
+++ ad-service/ad-common/src/main/java/com/springCloudAd/advice/CommonResponseDataAdvice.java
@@ -0,0 +1,75 @@
+package com.springCloudAd.advice;
+
+import com.springCloudAd.annotation.IgnoreResponseAdvice;
+import com.springCloudAd.vo.CommonResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class CommonResponseDataAdvice implements ResponseBodyAdvice<Object> {
+    /**
+     * 响应是否去拦截
+     * @param methodParameter
+     * @param aClass
+     * @return
+     */
+    @Override
+    @SuppressWarnings("all")
+    public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
+
+        /**
+         * getDeclaringClass 拿到类的申明
+         * isAnnotationPresent 被下面的注解标识
+         */
+        if (methodParameter.getDeclaringClass().isAnnotationPresent(
+                IgnoreResponseAdvice.class
+        )){
+            return false;
+        }
+        /**
+         * 方法上有注解
+         */
+        if (methodParameter.getMethod().isAnnotationPresent(
+                IgnoreResponseAdvice.class
+        )){
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 在写入响应之前
+     * @param o
+     * @param methodParameter
+     * @param mediaType
+     * @param aClass
+     * @param serverHttpRequest
+     * @param serverHttpResponse
+     * @return
+     */
+    @Nullable
+    @Override
+    @SuppressWarnings("all")
+    public Object beforeBodyWrite(@Nullable Object o, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+
+        //默认情况下面是 code为0 message为null
+        CommonResponse<Object> response=new CommonResponse<>(0,"");
+
+        if (null==o){
+            return response;
+        }else if (o instanceof CommonResponse){
+            response = (CommonResponse<Object>) o;
+        }else{
+            response.setData(o);
+        }
+
+        return response;
+    }
+}
---
+++ ad-service/ad-common/src/main/java/com/springCloudAd/vo/CommonResponse.java
@@ -0,0 +1,24 @@
+package com.springCloudAd.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 统一响应
+ */
+@Data                       //Get Set方法
+@NoArgsConstructor          //无参构造
+@AllArgsConstructor         //带参构造
+public class CommonResponse<T> implements Serializable {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResponse(Integer code,String message){
+        this.code=code;
+        this.message=message;
+    }
+}
