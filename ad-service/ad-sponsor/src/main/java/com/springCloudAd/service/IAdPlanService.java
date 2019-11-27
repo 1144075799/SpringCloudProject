f9c03c95574f9763542aec7cb262b0ae9d20c5b2
1144075799 <1144075799@qq.com>
2019-11-27 14:15:04 +0800

    AdPlan接口

---
+++ ad-service/ad-sponsor/src/main/java/com/springCloudAd/service/IAdPlanService.java
@@ -0,0 +1,43 @@
+package com.springCloudAd.service;
+
+import com.springCloudAd.entity.AdPlan;
+import com.springCloudAd.exception.AdException;
+import com.springCloudAd.vo.AdPlanGetRequest;
+import com.springCloudAd.vo.AdPlanRequest;
+import com.springCloudAd.vo.AdPlanResponse;
+
+import java.util.List;
+
+public interface IAdPlanService {
+
+    /**
+     * 创建推广计划
+     * @param request
+     * @return
+     * @throws AdException
+     */
+    AdPlanResponse createPlan(AdPlanRequest request) throws AdException;
+
+    /**
+     * 获取推广计划
+     * @param request
+     * @return
+     * @throws AdException
+     */
+    List<AdPlan> getAdPlanByIds(AdPlanGetRequest request)throws AdException;
+
+    /**
+     * 更新推广计划
+     * @param request
+     * @return
+     * @throws AdException
+     */
+    AdPlanResponse updateAdPlan(AdPlanRequest request) throws AdException;
+
+    /**
+     * 删除推广计划
+     * @param request
+     * @throws AdException
+     */
+    void deleteAdPlan(AdPlanRequest request)throws AdException;
+}
