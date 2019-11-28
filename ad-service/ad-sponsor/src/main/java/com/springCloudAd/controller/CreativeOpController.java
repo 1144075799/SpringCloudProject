8ad100d71dda07a70d9f436d5969c532288b18dd
1144075799 <1144075799@qq.com>
2019-11-28 13:22:58 +0800

    控制器的实现

---
+++ ad-service/ad-sponsor/src/main/java/com/springCloudAd/controller/CreativeOpController.java
@@ -0,0 +1,29 @@
+package com.springCloudAd.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.springCloudAd.exception.AdException;
+import com.springCloudAd.service.ICreativeService;
+import com.springCloudAd.vo.CreativeRequest;
+import com.springCloudAd.vo.CreativeResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class CreativeOpController {
+
+    private final ICreativeService creativeService;
+
+    @Autowired
+    public CreativeOpController(ICreativeService creativeService) {
+        this.creativeService = creativeService;
+    }
+    @PostMapping("/create/creative")
+    public CreativeResponse createCreative(@RequestBody CreativeRequest request) throws AdException{
+        log.info("ad-sponsor:createCreative->{}", JSON.toJSONString(request));
+        return creativeService.createCreative(request);
+    }
+}
